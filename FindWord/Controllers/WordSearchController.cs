c97602226bade46bf6c0398a846d8901bd8fe31e
Mustafa Dikyar <mustafa.dikyar@setur.com.tr>
2023-02-07 12:30:17 +0300

    Add project files.

---
+++ FindWord/Controllers/WordSearchController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindWord.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WordSearchController : ControllerBase
+{
+    [HttpPost("/search-from-file")]
+    public IActionResult SearchFromFile(List<IFormFile> files, string word)
+    {
+        var includedFilesNames = new List<string>();
+
+        files.ForEach(file =>
+        {
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                string contents = reader.ReadToEnd();
+
+                if (contents.Contains(word))
+                    includedFilesNames.Add(file.FileName);
+            }
+        });
+
+        if (includedFilesNames.Count > 0)
+            return Ok(includedFilesNames);
+
+        return NotFound();
+    }
+
+    [HttpGet("/search-from-folder")]
+    public IActionResult SearchFromFolder(string word)
+    {
+        var includedFilesNames = new List<string>();
+
+        string folderPath = @"C:\AirMirData";
+        string[] filePaths = Directory.GetFiles(folderPath);
+
+        foreach (string filePath in filePaths)
+        {
+            StreamReader sr = System.IO.File.OpenText(filePath);
+            string contents = sr.ReadToEnd();
+
+            if (contents.Contains(word))
+            {
+                sr.Close();
+                includedFilesNames.Add(filePath);
+            }
+            else
+                sr.Close();
+        }
+
+        if (includedFilesNames.Count > 0)
+            return Ok(includedFilesNames);
+
+        return NotFound();
+    }
+}
