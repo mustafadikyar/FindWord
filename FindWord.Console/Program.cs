2354a671b09f8bd6a178b0c333483491dce09bf6
Mustafa Dikyar <mustafa.dikyar@setur.com.tr>
2023-02-07 12:47:23 +0300

    Console app has been added.

---
+++ FindWord.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿string word;
+Console.Write("Enter a word: ");
+word = Console.ReadLine();
+
+var includedFilesNames = new List<string>();
+
+string folderPath = @"C:\AirMirData";
+string[] filePaths = Directory.GetFiles(folderPath);
+
+foreach (string filePath in filePaths)
+{
+    StreamReader sr = System.IO.File.OpenText(filePath);
+    string contents = sr.ReadToEnd();
+
+    if (contents.Contains(word))
+    {
+        sr.Close();
+        includedFilesNames.Add(filePath);
+    }
+    else
+        sr.Close();
+}
+
+if (includedFilesNames.Count > 0)
+    includedFilesNames.ForEach(fileName => Console.WriteLine(fileName + "\n"));
+else
+    Console.WriteLine("File Not Found");
+
+Console.ReadLine();
